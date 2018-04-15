b237efcfe864c2b47c626a51be7b265cd9b25cc1
freddy\Роман <promailwork@mail.ru>
2018-04-15 19:25:57 +0300

    Правки

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ class Vector {
 
     plus(vector) {
         if (vector instanceof Vector) {
-          return new Vector(this.x + vector.x, this.y + vector.y);
+            return new Vector(this.x + vector.x, this.y + vector.y);
         }
         throw new Error('Можно прибавлять к вектору только вектор типа Vector');
     }
@@ -47,21 +47,14 @@ class Actor {
     }
 
     isIntersect(actor) {
-        // лучше сначала проверить аргументы функции, а потом писать основной код
-        if (actor && actor instanceof Actor) {
-            if (actor === this) {
-                return false;
-            }
-            // зачем эти рассчёты?
-            let centerX = (actor.right + actor.left) / 2;
-            let centerY = (actor.bottom + actor.top) / 2;
-            // в таких случаях лучше писать прсто return <выражение из if>
-            if (this.left < centerX && centerX < this.right && this.top < centerY && centerY < this.bottom) {
-                  return true;
-            }
+        if (!(actor instanceof Actor)) {
+            throw new Error('Можно передавать только объекты типа Actor');
+        }
+
+        if (actor === this) {
             return false;
         }
-        throw new Error('Можно передавать только объекты типа Actor');
+        return this.right > actor.left && this.left < actor.right && this.bottom > actor.top && this.top < actor.bottom;
     }
 
 }
@@ -72,17 +65,7 @@ class Level{
         this.actors = actors;
         this.player = actors.find(item => item.type === 'player');
         this.height = this.grid.length;
-        this.width = 0;
-
-        // let of редко используется, чаще просто цикл или forEach
-        // в данном случае лучше использовать reduce или Math.max + map
-        for (let elem of this.grid) {
-            if (elem) {
-                if (this.width < elem.length) {
-                    this.width = elem.length;
-                }
-            }
-        }
+        this.width = Math.max(0, ...this.grid.map(cell => cell.length));
         this.status = null;
         this.finishDelay = 1;
     }
@@ -92,54 +75,51 @@ class Level{
     }
 
     actorAt(actor) {
-        if (actor && actor instanceof Actor) {
-            return this.actors.find(elem => actor.isIntersect(elem));
-            // else не нужен, т.к. в if return
-        } else {
-            // проверки аргументов лучше делать в начале функции
+        if (!actor || !(actor instanceof Actor)) {
             throw new Error('Можно передавать только объекты типа Actor и аргумент не может быть пустым');
         }
+        return this.actors.find(elem => actor.isIntersect(elem));
     }
 
     obstacleAt(pos, size) {
-        if ((pos instanceof Vector) || (size instanceof Vector)) {
-            const topBorder = pos.y;
-            const rightBorder = pos.x + size.x;
-            const bottomBorder = pos.y + size.y;
-            const leftBorder = pos.x;
+        if (!(pos instanceof Vector) || !(size instanceof Vector)) {
+            throw new Error('Можно передавать только объекты типа Vector');
+        }
 
-            if (leftBorder < 0 || topBorder < 0 || rightBorder > this.width) {
-                return 'wall'
-            }
+        const topBorder = pos.y;
+        const rightBorder = pos.x + size.x;
+        const bottomBorder = pos.y + size.y;
+        const leftBorder = pos.x;
 
-            if (bottomBorder > this.height) {
-                return 'lava';
-            }
+        if (leftBorder < 0 || topBorder < 0 || rightBorder > this.width) {
+            return 'wall'
+        }
 
-            // округлённые значение лучше записать в переменне, чтобы не округлять на каждой итерации цикла
-            for (let y = Math.floor(topBorder); y < Math.ceil(bottomBorder); y++) {
-                for (let x = Math.floor(leftBorder); x < Math.ceil(rightBorder); x++) {
-                    const fieldType = this.grid[y][x];
-                    if (fieldType) {
-                        return fieldType;
-                    }
+        if (bottomBorder > this.height) {
+            return 'lava';
+        }
+
+        const minTop = Math.floor(topBorder);
+        const maxBottom = Math.ceil(bottomBorder);
+        const minLeft = Math.floor(leftBorder);
+        const maxRight = Math.ceil(rightBorder);
+
+        for (let y = minTop; y < maxBottom; y++) {
+            for (let x = minLeft; x < maxRight; x++) {
+                const fieldType = this.grid[y][x];
+                if (fieldType) {
+                    return fieldType;
                 }
             }
-        } else {
-            // проверки аргументов лучше делать в начале функции
-            throw new Error('Можно передавать только объекты типа Vector');
         }
     }
 
     removeActor(actor) {
-        // если заменить на цикл код станет проще
-        let i = 0;
-        for (let elem of this.actors) {
-            if (actor === elem) {
+        for (let i = 0; i < this.actors.length; i++) {
+            if (actor === this.actors[i]) {
                 this.actors.splice(i,1);
-                // тут можно добавить return чтобы остановить перебор элементов когда нужный уже удалён
+                return;
             }
-            i++;
         }
     }
 
@@ -169,17 +149,11 @@ class Level{
 }
 
 class LevelParser {
-    // здесь можно добавить значение по-умолчанию
-    constructor(dictionary) {
-        // тут лучше создать копию объекта, чтобы поле нельзя было изменить извне
-        this.dictionary = dictionary;
+    constructor(dictionary = {}) {
+        this.dictionary = Object.assign({}, dictionary);
     }
 
     actorFromSymbol(symbol) {
-        // проверка лишняя
-        if (!symbol) {
-            return undefined;
-        }
         return this.dictionary[symbol];
     }
 
@@ -190,51 +164,23 @@ class LevelParser {
         if (symbol === '!') {
             return 'lava';
         }
-        // лишняя строчка, фукнция, возвращает undefined, если не указано другое
-        return undefined;
     }
 
     createGrid(arrString) {
-        // значение присваивается переменной один раз, поэтому лучше использовать const
-        let arr = [];
-        let bigArr = [];
-        let i = 0;
-        // более изящное решение - использовать map 2 раза
-        if (arrString.length != 0) {
-            for (let key of arrString) {
-                for (let symbol of key) {
-                    arr.push(this.obstacleFromSymbol(symbol));
-                }
-                bigArr[i] = arr;
-                arr = [];
-                i++;
-            }
-            return bigArr;
-        }
-        return [];
+        return arrString.map(row => row.split('').map(cell => this.obstacleFromSymbol(cell)));
     }
 
     createActors(arrString) {
-        // значение присваивается переменной один раз, поэтому лучше использовать const
-        let arr = [];
-        let i = 0;
-        let j = 0;
+        const arr = [];
         if (arrString.length != 0 && this.dictionary !== undefined) {
-            // for let of лучше заменить простым циклом или forEach
-            for (let key of arrString) {
-                for (let obj of key) {
-                    // тут лучше объявить несколько переменных,
-                    // например чтобы не писать везде this.dictionary[obj]
-                    if (obj in this.dictionary) {
-                     if (Actor.isPrototypeOf(this.dictionary[obj]) || this.dictionary[obj] === Actor) {
-                        arr.push(new this.dictionary[obj](new Vector(i, j)));
-                     }
+            arrString.forEach((row, rowIndex) => {
+                row.split('').forEach((cell, cellIndex) => {
+                    const dictionary = this.actorFromSymbol(cell);
+                    if (Actor.isPrototypeOf(dictionary) || dictionary === Actor) {
+                        arr.push(new dictionary(new Vector(cellIndex, rowIndex)));
                     }
-                    i++;
-                }
-                i = 0;
-                j++;
-            }
+                });
+            });
             return arr;
         }
         return [];
@@ -247,7 +193,7 @@ class LevelParser {
 }
 
 class Fireball extends Actor {
-    constructor(pos, speed) {
+    constructor(pos = new Vector(0, 0), speed = new Vector(0, 0)) {
         super(pos, new Vector(1, 1), speed);
     }
 
@@ -274,19 +220,19 @@ class Fireball extends Actor {
 }
 
 class HorizontalFireball extends Fireball {
-    constructor(pos) {
+    constructor(pos = new Vector(0, 0)) {
         super(pos, new Vector(2, 0));
     }
 }
 
 class VerticalFireball extends Fireball {
-    constructor(pos) {
+    constructor(pos = new Vector(0, 0)) {
         super(pos, new Vector(0, 2));
     }
 }
 
 class FireRain extends Fireball {
-    constructor(pos) {
+    constructor(pos = new Vector(0, 0)) {
         super(pos, new Vector(0, 3));
         this.basePos = pos;
     }
@@ -297,8 +243,7 @@ class FireRain extends Fireball {
 }
 
 class Coin extends Actor {
-    // лучше не опускать аргументы конструктора Vector - его может кто-нибудь поменять, а код сломается ваш
-    constructor(pos = new Vector()) {
+    constructor(pos = new Vector(0, 0)) {
         super(pos.plus(new Vector(0.2,0.1)), new Vector(0.6, 0.6));
         this.springSpeed = 8;
         this.springDist = 0.07;
@@ -329,7 +274,7 @@ class Coin extends Actor {
 }
 
 class Player extends Actor {
-    constructor(pos = new Vector()) {
+    constructor(pos = new Vector(0, 0)) {
         super(pos.plus(new Vector(0, -0.5)), new Vector(0.8, 1.5));
     }
 
